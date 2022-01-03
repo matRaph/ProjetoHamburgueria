0dede2e3d23f9fe16d161f7f7b76dd4feffb3c7b
Raphael Matias <42880393+matRaph@users.noreply.github.com>
2022-01-02 23:40:26 -0300

    Classe Pessoa e subclasse Funcionario.

---
+++ ProjetoHamburgueria/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoHamburgueria
+{
+    class Pessoa
+    {
+        private string nome;
+        private int cpf;
+        private int idade;
+        
+        public Pessoa(string nome, int cpf, int idade)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.idade = idade;
+        }
+
+        class Funcionario
+        {
+            private int idFuncionario;
+            private float salario;
+        }
+    }
+}
--- ProjetoHamburgueria/Program.cs
+++ ProjetoHamburgueria/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoHamburgueria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
